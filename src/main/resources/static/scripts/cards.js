8454b0b74abfc7586beb737d5f6a4bb4edc79357
Gothman240 <paez.federico96@gmail.com>
2023-06-21 20:08:13 -0300

    task-5

---
+++ src/main/resources/static/scripts/cards.js
@@ -0,0 +1,70 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return {
+            showSide: false,
+            client: [],
+            cards: [],
+            debitCards: [],
+            creditCards: []
+        }
+    },
+    created(){
+        this.loadData();
+    },
+    methods: {
+        showSideBar(){
+            this.showSide = !this.showSide;
+        },
+        loadData(){
+            axios.get("http://localhost:8080/api/clients/1")
+            .then(response  => {
+                this.client = response.data
+                this.cards = response.data.cards.sort((a, b) => a.id - b.id)
+                this.debitCards = this.cards.filter(card => card.type === "DEBIT")
+                this.creditCards = this.cards.filter(card => card.type === "CREDIT")
+                console.log(this.client)
+            })
+            .catch(err => console.log(err))
+        },
+        formatDate(date){
+            const oldDate = date;
+            const dateSplit = oldDate.split("-")
+
+            const month = dateSplit[1];
+            const year = dateSplit[0].substring(2);
+
+            const newDate = `${year}-${month}`
+
+            return newDate;
+        },
+        colorCard(color){
+            console.log(color === "TITANIUM")
+            if(color === "TITANIUM"){
+                return "grey"
+            }
+            if(color === "GOLD"){
+                return "yellow"
+            }
+            if(color === "SILVER"){
+                return "grey"
+            }
+        },
+        secondaryColorCard(color){
+            console.log(color === "TITANIUM")
+            if(color === "TITANIUM"){
+                return "greydark"
+            }
+            if(color === "GOLD"){
+                return "limedark"
+            }
+            if(color === "SILVER"){
+                return "grey"
+            }
+        }
+    },
+    computed: {
+       
+    }
+}).mount("#app")
